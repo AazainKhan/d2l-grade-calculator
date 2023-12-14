a4c01d3056d980cdd2dc4d4e761d8231ae8922bf
Aazain <43759637+Aazainkhan@users.noreply.github.com>
2023-12-14 05:04:41 -0500

    Refactor grade calculation logic and add message listener

--- background.js
+++ background.js
@@ -1,5 +1,21 @@
-// background.js
+// Message listener for receiving messages from content scripts
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === "calculate_grade") {
+        // If calculateTotalGrade is asynchronous, you can use a Promise
+        calculateTotalGrade().then(totalGrade => {
+            // Send the total grade back to the content script
+            sendResponse({ totalGrade });
+        });
 
-chrome.runtime.onInstalled.addListener(function() {
-    console.log("Grade Calculator extension installed.");
+        // Return true to indicate you wish to send a response asynchronously
+        return true;
+    }
 });
+
+// Example function for calculating the total grade (you should implement your logic here)
+function calculateTotalGrade() {
+    // Implement your grade calculation logic here
+    // Replace this with your actual calculation
+    const totalGrade = 85.5; // Example total grade
+    return Promise.resolve(totalGrade); // Return a Promise that resolves with the total grade
+}
--- content.js
+++ content.js
@@ -1,7 +1,3 @@
-// content.js
-console.log("Content script running");
-
-// Function with partial logic
 function calculateGradeTotal() {
     let gradesTable = document.querySelector('table');
     if (!gradesTable) {
@@ -10,69 +6,41 @@ function calculateGradeTotal() {
     }
     console.log("Grades table found");
 
-    // Part 2: Add more logic here, testing each part
     let totalWeightAchieved = 0;
     let totalMaxWeight = 0;
 
     gradesTable.querySelectorAll('tr').forEach((row, index) => {
-        if (index === 0) return;
+        if (index === 0) return; // Skip header row
+
+        if (row.classList.contains('d_ggl1')) return;
 
         let weightCell = row.cells[2];
-        if (weightCell) {
-            let weights = weightCell.textContent.split('/');
-            if (weights.length === 2) {
-                let weightAchieved = parseFloat(weights[0].trim());
-                let maxWeight = parseFloat(weights[1].trim());
-                if (!isNaN(weightAchieved) && !isNaN(maxWeight)) {
-                    totalWeightAchieved += weightAchieved;
-                    totalMaxWeight += maxWeight;
-                }
+        if (weightCell && !weightCell.textContent.includes('/')) return; // Skip rows without weights
+
+        let weights = weightCell.textContent.split('/');
+        if (weights.length === 2) {
+            let weightAchieved = parseFloat(weights[0].trim());
+            let maxWeight = parseFloat(weights[1].trim());
+            if (!isNaN(weightAchieved) && !isNaN(maxWeight)) {
+                totalWeightAchieved += weightAchieved;
+                totalMaxWeight += maxWeight;
             }
         }
     });
 
     let totalPercentage = totalMaxWeight ? (totalWeightAchieved / totalMaxWeight) * 100 : 0;
 
-    // Find the header row to determine the column index for "Weight Achieved"
-    let headerRow = gradesTable.querySelector('tr');
-    let weightAchievedColumnIndex = -1;
+    // Insert the calculated total into the table
+    let newRow = gradesTable.insertRow();
+    let cell = newRow.insertCell(0);
 
-    if (headerRow) {
-        Array.from(headerRow.cells).forEach((cell, index) => {
-            if (cell.textContent.includes("Weight Achieved")) {
-                weightAchievedColumnIndex = index;
-            }
-        });
-    }
+    // Set the cell's colSpan to match the number of header columns
+    let headerColumns = gradesTable.querySelectorAll('th').length;
+    cell.colSpan = headerColumns;
 
-    if (weightAchievedColumnIndex !== -1) {
-        // Insert a new row with the total percentage under the "Weight Achieved" column
-        let newRow = gradesTable.insertRow();
-        
-        // Create the first cell with "Total grade" text
-        let totalGradeCell = newRow.insertCell();
-        totalGradeCell.innerHTML = "<b>Total grade</b>";
-
-        // Create empty cells to align with the header structure
-        for (let i = 1; i < weightAchievedColumnIndex; i++) {
-            newRow.insertCell();
-        }
-
-        // Create a cell for the total percentage value
-        let percentageCell = newRow.insertCell();
-        percentageCell.innerHTML = `<b>${totalPercentage.toFixed(2)}%</b>`;
-        percentageCell.style.textAlign = "right"; // Align to the right
-
-        // Create a cell for empty space to align with the remaining columns
-        newRow.insertCell();
-
-        // Fill the remaining cells to match the header structure
-        let remainingColumns = headerRow.cells.length - weightAchievedColumnIndex - 2;
-        for (let i = 0; i < remainingColumns; i++) {
-            newRow.insertCell();
-        }
-    }
+    // Align the content to the left
+    cell.innerHTML = `<b>Total Grade: ${totalPercentage.toFixed(2)}%</b>`;
+    cell.style.textAlign = "left";
 }
 
-// Execute the function
 calculateGradeTotal();
